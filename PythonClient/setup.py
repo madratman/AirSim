8b02431903be91e5c951965a5448682fe28e204f
madratman <ratneshmadaan@gmail.com>
2019-11-06 11:58:16 -0800

    move all unreal calls ASimModeWorldBase::Tick

--- PythonClient/setup.py
+++ PythonClient/setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/microsoft/airsim",
     packages=setuptools.find_packages(),
-	license='MIT',
+    license='MIT',
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
